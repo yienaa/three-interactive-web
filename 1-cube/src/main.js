f5fb2e68f0be1be8a691abd1d424e468971ce8a9
Nayeong Lee <98475086+yienaa@users.noreply.github.com>
2025-01-14 23:41:53 +0900

    test lil-gui, orbitcontrol

--- 1-cube/src/main.js
+++ 1-cube/src/main.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import GUI from 'lil-gui'
 
 window.addEventListener('load', function () {
   init();
@@ -7,6 +9,9 @@ window.addEventListener('load', function () {
 
 
 function init() {
+  const options = {
+    color: 0x00ff00,
+  }
   const renderer = new THREE.WebGLRenderer({
     // alpha: true,
     antialias: true
@@ -17,6 +22,26 @@ function init() {
   const scene = new THREE.Scene(2, 2, 2);
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 500);
 
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.autoRotate = true;
+  controls.autoRotateSpeed = 10;
+  controls.enableDamping = true;
+  // controls.dampingFactor = 0.25;
+  controls.enableZoom = true;
+  // controls.enablePan = true;
+  controls.maxDistance = 10;
+  controls.minDistance = 2;
+
+  controls.minPolarAngle = Math.PI / 4;
+  controls.maxPolarAngle = Math.PI / 2;
+
+  controls.maxAzimuthAngle = Math.PI / 4;
+  controls.minAzimuthAngle = -Math.PI / 4;
+
+
+  
+  // const axisHelper = new THREE.AxesHelper(5);
+  // scene.add(axisHelper);
 
   // const geometry = new THREE.BoxGeometry();
   const cubegeometry = new THREE.IcosahedronGeometry(1);
@@ -73,18 +98,20 @@ function init() {
 
   function render () {
     const elapsedTime = clock.getElapsedTime();
-    requestAnimationFrame(render);
     // cube.rotation.x = Date.now() * 0.0001;
     // cube.rotation.x = clock.getElapsedTime();
     // cube.rotation.x += clock.getDelta();
     // cube.rotation.y += 0.01;
 
-    cube.rotation.x = elapsedTime;
-    cube.rotation.y = elapsedTime;
+    // cube.rotation.x = elapsedTime;
+    // cube.rotation.y = elapsedTime;
 
-    skelcton.rotation.x = elapsedTime * 1.5;
-    skelcton.rotation.y = elapsedTime * 1.5;
+    // skelcton.rotation.x = elapsedTime * 1.5;
+    // skelcton.rotation.y = elapsedTime * 1.5;
     renderer.render(scene, camera);
+    controls.update();
+    requestAnimationFrame(render);
+
   }
 
   // 어떤 환경에서든 동일한 fps를 유지하기 위해
@@ -99,6 +126,24 @@ function init() {
 
 
   window.addEventListener('resize', function () {
+    controls.update();
     handleResize();
   });
+
+
+  const gui = new GUI();
+  // gui.add(cube.rotation, 'y', 0, Math.PI * 2, 0.1);
+  gui
+  .add(cube.position, 'y')
+  .min(-3)
+  .max(3)
+  .step(0.1)
+  .name('positionY');
+
+  gui.add(cube, 'visible');
+  gui.addColor(cube.material, 'color');
+  gui.addColor(options, 'color').onChange((value) => {
+    cube.material.color.set(value);
+  });
+
 } 
